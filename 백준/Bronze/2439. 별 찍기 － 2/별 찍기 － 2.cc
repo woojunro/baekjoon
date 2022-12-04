34271a20df861de36d47ded03a06a562d36e3644
Woojun Ro <58510013+woojunro@users.noreply.github.com>
2022-12-04 16:29:10 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	int n;
+	std::cin >> n;
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < n - i; j++) std::cout << ' ';
+		for (int j = 0; j < i; j++) std::cout << '*';
+		std::cout << std::endl;
+	}
+	
+	return 0;
+}

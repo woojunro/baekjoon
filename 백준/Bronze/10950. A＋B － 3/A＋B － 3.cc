2037cb6c7c3964b10bbfd7ae7fa810006956cc0b
Woojun Ro <58510013+woojunro@users.noreply.github.com>
2022-12-04 14:58:37 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	int t, a, b;
+	
+	std::cin >> t;
+	
+	for (int i = 0; i < t; i++) {
+		std::cin >> a >> b;
+		std::cout << a + b << std::endl;
+	}
+	
+	return 0;
+}

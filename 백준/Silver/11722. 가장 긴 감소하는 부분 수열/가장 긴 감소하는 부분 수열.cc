398347e118a0424cc29b1b61e6c0f79fd0c336da
Woojun Ro <58510013+woojunro@users.noreply.github.com>
2023-01-24 17:32:11 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#define MAX 1000
+
+int DP[MAX + 1];
+int A[MAX + 1];
+
+int main() {
+	int N, max;
+	std::cin >> N;
+	
+	A[0] = 1001;
+	for (int i = 1; i <= N; i++) {
+		std::cin >> A[i];
+	}
+	
+	for (int i = 1; i <= N; i++) {
+		max = -1;
+		for (int j = 0; j < i; j++) {
+			if (A[j] > A[i] && DP[j] > max) {
+				max = DP[j];
+			}
+		}
+		DP[i] = max + 1;
+	}
+	
+	max = -1;
+	for (int i = 1; i <= N; i++) {
+		if (DP[i] > max) {
+			max = DP[i];
+		}
+	}
+	
+	std::cout << max << std::endl;
+	
+	return 0;
+}

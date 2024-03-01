c6b1a7041253ce4183274cb13a8a785e055a64aa
Woojun Ro <58510013+woojunro@users.noreply.github.com>
2024-03-01 20:37:51 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,32 @@
+WITH SCORE_TABLE AS (
+    SELECT
+        EMP_NO,
+        AVG(SCORE) AS SCORE
+    FROM
+        HR_GRADE
+    GROUP BY
+        EMP_NO
+)
+
+SELECT
+    E.EMP_NO,
+    E.EMP_NAME,
+    CASE
+        WHEN S.SCORE >= 96 THEN 'S'
+        WHEN S.SCORE >= 90 THEN 'A'
+        WHEN S.SCORE >= 80 THEN 'B'
+        ELSE 'C'
+    END AS GRADE,
+    CASE
+        WHEN S.SCORE >= 96 THEN E.SAL * 0.2
+        WHEN S.SCORE >= 90 THEN E.SAL * 0.15
+        WHEN S.SCORE >= 80 THEN E.SAL * 0.1
+        ELSE 0
+    END AS BONUS
+FROM
+    HR_EMPLOYEES AS E
+INNER JOIN
+    SCORE_TABLE AS S ON E.EMP_NO = S.EMP_NO
+ORDER BY
+    E.EMP_NO ASC
+;
